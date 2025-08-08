bf09548d9e43bfa38c4dd700fec60adf02fb9323
Frimi01 <82447464+Frimi01@users.noreply.github.com>
2025-08-08 13:38:25 +0200

    updated build script

--- build.sh
+++ build.sh
@@ -10,39 +10,77 @@ VERSION=$1
 BUILD_DIR=build
 REQUIRED_FILES=("bookmarks.json" "icon.ico" "LICENSE" "README.md")
 REQUIRED_DIRECTORIES=("public")
+STRIP=true
+if [ "$2" == "nostrip" ]; then
+    STRIP=false
+fi
 
-echo "Building version $VERSION..."
+buildPlatform() {
+  local GOOS="$1"
+  local GOARCH="$2"
+  local LABEL="FriBrowse-$VERSION-$GOOS-$GOARCH"
+  local OUTPUT_DIR="$BUILD_DIR/$LABEL"
+  local LDFLAGS=""
+  if [ "$GOOS" == "windows" ]; then
+	  LDFLAGS="-H=windowsgui"
+	  LABEL="$LABEL.exe"
+  fi
+  if [ "$STRIP" == "true" ]; then
+	  LDFLAGS="$LDFLAGS -s -w"
+  fi
+  
 
+	echo "Building $LABEL executable..."
+	mkdir -p "$OUTPUT_DIR"
+
+
+	GOOS="$GOOS" GOARCH="$GOARCH" go build -ldflags="$LDFLAGS" -o "$OUTPUT_DIR/$LABEL"
+
+	if [ $? -ne 0 ]; then
+		echo "❌ Failed to build $LABEL."
+	exit 1
+	fi
+
+
+	echo "Copying required directories to $BUILD_DIR..."
+	for directory in "${REQUIRED_DIRECTORIES[@]}"; do 
+	if [ ! -d "$directory" ]; then
+		echo "Error: Required directory '$directory' not found."
+		exit 1
+	fi
+
+	cp -r "$directory" "$OUTPUT_DIR/"
+	echo "  - Copied $directory directory to $OUTPUT_DIR/$directory"
+	done
+
+	echo "Copying required files to $OUTPUT_DIR..."
+	for file in "${REQUIRED_FILES[@]}"; do
+	if [ ! -f "$file" ]; then
+		echo "Error: Required file '$file' not found."
+		exit 1
+	fi
+
+	cp "$file" "$OUTPUT_DIR/"
+	echo "  - Copied $file file to $OUTPUT_DIR/$file"
+	done  
+
+	echo "Zipping directory $OUTPUT_DIR"
+	zip -r "$OUTPUT_DIR.zip" "$OUTPUT_DIR"
+
+	echo "Done building for $GOOS/$GOARCH. Artifacts in $OUTPUT_DIR"
+}
+
+
+echo "Preparing build directory..."
 rm -rf "$BUILD_DIR"
 mkdir -p "$BUILD_DIR"
 
-echo "Building Linux executable..."
-GOOS=linux GOARCH=amd64 go build -o "$BUILD_DIR/FriBrowse-$VERSION-linux-amd64"
 
-echo "Building Windows executable..."
-GOOS=windows GOARCH=amd64 go build -o "$BUILD_DIR/FriBrowse-$VERSION-windows-amd64.exe"
+echo "Building version $VERSION..."
+buildPlatform "linux" "amd64" 
+buildPlatform "windows" "amd64" 
+# buildPlatform "darwin" "amd64"
 
-echo "Copying required directories to $BUILD_DIR..."
-for directory in "${REQUIRED_DIRECTORIES[@]}"; do 
-  if [ ! -d "$directory" ]; then
-    echo "Error: Required directory '$directory' not found."
-    exit 1
-  fi
-
-  echo "Copying $directory directory to $BUILD_DIR/$directory"
-  cp -r "$directory" "$BUILD_DIR/"
-done
-
-echo "Copying required files to $BUILD_DIR..."
-for file in "${REQUIRED_FILES[@]}"; do
-  if [ ! -f "$file" ]; then
-    echo "Error: Required file '$file' not found."
-    exit 1
-  fi
-
-  cp "$file" "$BUILD_DIR/"
-  echo "  - Copied $file"
-done
 
 echo "All builds are done! Artifacts are in the $BUILD_DIR folder."
 
